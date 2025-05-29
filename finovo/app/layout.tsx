8e6c3d16042511df550e266b6f05d0a9d6ab0cc4
Shreya <shreya16180@gmail.com>
2025-05-29 13:27:43 +0530

    signin, signup route setup

--- finovo/app/layout.tsx
+++ finovo/app/layout.tsx
@@ -1,24 +1,24 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-// import { Inter, IBM_Plex_Serif } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
+import { Inter, IBM_Plex_Serif } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-	variable: "--font-geist-sans",
-	subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-	variable: "--font-geist-mono",
-	subsets: ["latin"],
-});
-
-// const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
-// const ibmPlexSerif = IBM_Plex_Serif({
+// const geistSans = Geist({
+// 	variable: "--font-geist-sans",
 // 	subsets: ["latin"],
-// 	weight: ["400", "700"],
-// 	variable: "--font-ibm-plex-serif",
 // });
+
+// const geistMono = Geist_Mono({
+// 	variable: "--font-geist-mono",
+// 	subsets: ["latin"],
+// });
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+const ibmPlexSerif = IBM_Plex_Serif({
+	subsets: ["latin"],
+	weight: ["400", "700"],
+	variable: "--font-ibm-plex-serif",
+});
 
 export const metadata: Metadata = {
 	title: "Finovo | Easy banking",
@@ -35,9 +35,12 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body
+			{/* <body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 			>
+				{children}
+			</body> */}
+			<body className={`${inter.variable} ${ibmPlexSerif.variable}`}>
 				{children}
 			</body>
 		</html>
